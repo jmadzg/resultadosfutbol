46e9adb5ac6517bf9bca2cd5f2d14609054c5e92
Juan Manuel Díaz <juanma_dh14@hotmail.com>
2016-06-02 10:38:07 +0200

    Añadiendo nuevo template

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -3,26 +3,13 @@ import requests
 import json
 from bottle import request, get, post, run, debug, route, template, error, TEMPLATE_PATH, default_app, static_file
 import bottle
-import os
-import mybottleapp
+
 application=default_app()
 
 
 
 
 
-os.environ['OPENSHIFT_REPO_DIR']=""
-
-
-
-
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
- 
-@route('/')
-def index():
-    return '<strong>Hello World!</strong>'
 
 
 
@@ -44,6 +31,4 @@ TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/
 
 
 
-run(host='0.0.0.0', port=8080)
 
-debug='TRUE'
